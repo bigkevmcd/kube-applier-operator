dd915b0d79344ec2c5ada52714a98b95f357a485
Kevin McDermott <bigkevmcd@gmail.com>
2020-03-21 07:19:20 +0000

    Initial commit fresh from being generated.

---
+++ pkg/apis/applier/group.go
@@ -0,0 +1,6 @@
+// Package applier contains applier API versions.
+//
+// This file ensures Go source parsers acknowledge the applier package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package applier
---
+++ pkg/apis/applier/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the applier v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=applier.bigkevmcd.com
+package v1alpha1
